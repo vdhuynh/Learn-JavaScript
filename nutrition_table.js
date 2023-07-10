7df1065ebc57ae5751c5c0a8e94d01ad739a831c
Van Huynh <27980695+vdhuynh@users.noreply.github.com>
2023-07-09 21:38:46 -0500

    Create nutrition_table.js

---
+++ nutrition_table.js
@@ -0,0 +1,11 @@
+/**
+ * @param {string} label
+ * @param {string} value
+ */
+export function renderTableRow(label, value) {
+    console.log(label, value);
+    return `<tr>
+    <td>${label} here</td>
+    <td>${value} here</td>
+</tr>`
+}
