6f99f295f46dd7ca2b8d1a5333487b8bc5d7210f
Van Huynh <27980695+vdhuynh@users.noreply.github.com>
2023-07-09 21:46:51 -0500

    Create number_to_string_conversion.js

---
+++ number_to_string_conversion.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number} number
+ */
+function convertNumberToString(number) {
+    return number.toString();
+}
+
+// Sample usage - do not modify
+console.log(convertNumberToString(42)); // "42"
+console.log(convertNumberToString(97)); // "97"
+console.log(convertNumberToString(11)); // "11"
