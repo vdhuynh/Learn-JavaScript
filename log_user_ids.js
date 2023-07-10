1a056e17188f164eee154792b63c289eb30f6eca
Van Huynh <27980695+vdhuynh@users.noreply.github.com>
2023-07-09 23:13:19 -0500

    Create log_user_ids.js

---
+++ log_user_ids.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} userIds
+ */
+function logUserIds(userIds) {
+    userIds.forEach(id => {
+        console.log(id);
+    });
+}
+
+// Sample usage - do not modify
+logUserIds([10, 15, 14]); // should log every userId to the console
