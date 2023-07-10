67a2893b2351f0b85538ced3933acb85bdeda06d
Van Huynh <27980695+vdhuynh@users.noreply.github.com>
2023-07-10 11:13:32 -0500

    Create product_details.js

---
+++ product_details.js
@@ -0,0 +1,10 @@
+function getProductDetails() {
+    return {
+    id: 3,
+    title: 'yes',
+    inStock: true
+    }
+};
+
+// Sample usage - do not modify
+console.log(getProductDetails()); // the object you define
