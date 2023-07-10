e5b0d1f48be59c6c7f8c48968f79729c058a3a29
Van Huynh <27980695+vdhuynh@users.noreply.github.com>
2023-07-09 21:26:28 -0500

    Create lower_case_string.js

---
+++ lower_case_string.js
@@ -0,0 +1,10 @@
+/**
+ * @param {string} name
+ */
+function lowerName(name) {
+    return name.toLowerCase();
+}
+
+// Sample usage - do not modify
+console.log(lowerName("Sam")); // "sam"
+console.log(lowerName("ALEX")); // "alex"
