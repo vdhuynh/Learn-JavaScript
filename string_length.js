717d8dedfe84ba109ed4c19651523e5fd59ada2c
Van Huynh <27980695+vdhuynh@users.noreply.github.com>
2023-07-09 21:25:20 -0500

    Create string_length.js

---
+++ string_length.js
@@ -0,0 +1,11 @@
+/**
+ * @param {string} str
+ */
+function getCharCount(str) {
+    return str.length;
+}
+
+// Sample usage - do not modify
+console.log(getCharCount("Sam")); // 3
+console.log(getCharCount("Alex 123")); // 8
+console.log(getCharCount("Charley is here")); // 15
