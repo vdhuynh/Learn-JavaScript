4134a9472fa2fff5b77ee63df676170084dcadbd
Van Huynh <27980695+vdhuynh@users.noreply.github.com>
2023-07-10 13:38:36 -0500

    Create string_sizes_arrow.js

---
+++ string_sizes_arrow.js
@@ -0,0 +1,9 @@
+/**
+ * @param {string[]} strings
+ */
+const getStringSizes = strings => strings.map(string => string.length);
+
+// Sample usage - do not modify
+console.log(getStringSizes(["a", "abc"])); // [1, 3]
+console.log(getStringSizes(["Sam", "Alex", "Charlie"])); // [3, 4, 7]
+console.log(getStringSizes(["Hello", "Blue"])); // [5, 4]
