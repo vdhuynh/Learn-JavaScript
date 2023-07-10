8a96ba3c60a108aac7d7930fdd50d4af0258849c
Van Huynh <27980695+vdhuynh@users.noreply.github.com>
2023-07-09 21:28:20 -0500

    Create get_last_character.js

---
+++ get_last_character.js
@@ -0,0 +1,11 @@
+/**
+ * @param {string} name
+ */
+function getLastCharacter(name) {
+    return name[name.length - 1];
+}
+
+// Sample usage - do not modify
+console.log(getLastCharacter("Sam")); // "m"
+console.log(getLastCharacter("Alex")); // "x"
+console.log(getLastCharacter("Charley")); // "y"
