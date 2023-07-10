05721a8e3b9fc47f63f50714527f04143447931e
Van Huynh <27980695+vdhuynh@users.noreply.github.com>
2023-07-10 12:04:20 -0500

    Create improved_sum.js

---
+++ improved_sum.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} a
+ * @param {number} b
+ */
+function sum(a = 0, b = 0) {
+    return a + b;
+}
+
+// Sample usage - do not modify
+console.log(sum(2, 4)); // 6
+console.log(sum(3, 6)); // 9
+console.log(sum(3)); // 3
+console.log(sum(5)); // 5
+console.log(sum()); // 0
