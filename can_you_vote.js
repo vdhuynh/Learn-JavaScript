f689c574142c38d896c94a36bafd910c7d2050ef
Van Huynh <27980695+vdhuynh@users.noreply.github.com>
2023-07-09 22:04:33 -0500

    Create can_you_vote.js

---
+++ can_you_vote.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number} age
+ */
+function canVote(age) {
+    return age >= 18;
+}
+
+// Sample usage - do not modify
+console.log(canVote(25)); // true
+console.log(canVote(18)); // true
+console.log(canVote(10)); // false
