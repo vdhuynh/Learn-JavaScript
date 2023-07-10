eca4dd240ea28b883228fa6a1dce2a6499e6bdaf
Van Huynh <27980695+vdhuynh@users.noreply.github.com>
2023-07-09 21:26:03 -0500

    Create shout_my_name.js

---
+++ shout_my_name.js
@@ -0,0 +1,11 @@
+/**
+ * @param {string} name
+ */
+function shoutMyName(name) {
+    return name.toUpperCase();
+}
+
+// Sample usage - do not modify
+console.log(shoutMyName("Sam")); // "SAM"
+console.log(shoutMyName("Charley")); // "CHARLEY"
+console.log(shoutMyName("alex")); // "ALEX"
