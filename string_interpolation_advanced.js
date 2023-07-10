2e07cdfa985c607aa68825012948050088242d7b
Van Huynh <27980695+vdhuynh@users.noreply.github.com>
2023-07-09 21:35:02 -0500

    Create string_interpolation_advanced.js

---
+++ string_interpolation_advanced.js
@@ -0,0 +1,11 @@
+/**
+ * @param {string} firstName
+ * @param {string} lastName
+ */
+function getFullName(firstName, lastName) {
+    return `${firstName} ${lastName}`;
+}
+
+// Sample usage - do not modify
+console.log(getFullName("Sam", "Doe")); // "Sam Doe"
+console.log(getFullName("Alex", "Blue")); // "Alex Blue"
