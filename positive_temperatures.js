98023d103ac245cc113410d3ba10e3f0dda32564
Van Huynh <27980695+vdhuynh@users.noreply.github.com>
2023-07-10 10:32:23 -0500

    Create positive_temperatures.js

---
+++ positive_temperatures.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} temperatures
+ */
+function getPositiveTemperatures(temperatures) {
+    return temperatures.filter(temperature => {
+        return temperature > 0 });
+};
+
+
+// Sample usage - do not modify
+console.log(getPositiveTemperatures([-5, 12, 3])); // [12, 3]
+console.log(getPositiveTemperatures([1, -3, -2, 4, 10])); // [1, 4, 10]
