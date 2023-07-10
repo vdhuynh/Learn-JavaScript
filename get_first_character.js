f1c228153300853341743b4055c15464959e999c
Van Huynh <27980695+vdhuynh@users.noreply.github.com>
2023-07-09 21:28:04 -0500

    Create get_first_character.js

---
+++ get_first_character.js
@@ -0,0 +1,10 @@
+/**
+ * @param {string} name
+ */
+function getFirstCharacter(name) {
+    return name[0];
+}
+
+// Sample usage - do not modify
+console.log(getFirstCharacter("Amsterdam")); // "A"
+console.log(getFirstCharacter("Japan")); // "J"
