f369f4777e94fa12026dee54ea4a58143d7d9507
Van Huynh <27980695+vdhuynh@users.noreply.github.com>
2023-07-09 22:27:35 -0500

    Create get_first_app.js

---
+++ get_first_app.js
@@ -0,0 +1,10 @@
+/**
+ * @param {array} apps
+ */
+function getFirstApp(apps) {
+    return apps[0];
+}
+
+// Sample usage - do not modify
+console.log(getFirstApp(["Chrome", "Clock", "Twitter"])); // "Chrome"
+console.log(getFirstApp(["Clock", "Contacts"])); // "Clock"
