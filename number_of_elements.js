9230a6d4e73df98b8ad06144ed63091302588f68
Van Huynh <27980695+vdhuynh@users.noreply.github.com>
2023-07-09 22:22:44 -0500

    Create number_of_elements.js

---
+++ number_of_elements.js
@@ -0,0 +1,10 @@
+/**
+ * @param {array} elements
+ */
+function getNumberOfElements(elements) {
+    return elements.length;
+}
+
+// Sample usage - do not modify
+console.log(getNumberOfElements(['a', 'b', 'c'])); // 3
+console.log(getNumberOfElements([])); // 0
