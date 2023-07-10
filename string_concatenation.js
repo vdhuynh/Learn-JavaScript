2c190ff77739b48a369c60000040fb976ba844f2
Van Huynh <27980695+vdhuynh@users.noreply.github.com>
2023-07-09 21:31:11 -0500

    Create string_concatenation.js

---
+++ string_concatenation.js
@@ -0,0 +1,11 @@
+/**
+ * @param {string} firstNameInitial
+ * @param {string} lastNameInitial
+ */
+function concatInitials(firstNameInitial, lastNameInitial) {
+    return firstNameInitial + lastNameInitial;
+}
+
+// Sample usage - do not modify
+console.log(concatInitials("J", "D")); // "JD"
+console.log(concatInitials("S", "B")); // "SB"
