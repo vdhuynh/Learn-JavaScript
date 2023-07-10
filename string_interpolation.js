7859e7b2f818b9e1519bbdda91cef7da6fc80b96
Van Huynh <27980695+vdhuynh@users.noreply.github.com>
2023-07-09 21:34:09 -0500

    Create string_interpolation.js

---
+++ string_interpolation.js
@@ -0,0 +1,10 @@
+/**
+ * @param {string} name
+ */
+function sayHello(name) {
+    return `Hello ${name}`;
+}
+
+// Sample usage - do not modify
+console.log(sayHello("Alex")); // "Hello Alex"
+console.log(sayHello("Sam")); // "Hello Sam"
