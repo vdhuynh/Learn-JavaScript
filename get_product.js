efacdec1736719e46f735e1fb762a0c9974d9131
Van Huynh <27980695+vdhuynh@users.noreply.github.com>
2023-07-11 12:57:14 -0500

    Create get_product.js

---
+++ get_product.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} a
+ * @param {number} b
+ */
+const getProduct = (a, b) => {
+    console.log({a, b});
+    let product = a * b;
+    console.log({product});
+    return product;
+}
+
+// Sample usage - do not modify
+getProduct(2, 3);
