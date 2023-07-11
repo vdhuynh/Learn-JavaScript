888e11309cf8f7052b70042e2de5944fde77fb5d
Van Huynh <27980695+vdhuynh@users.noreply.github.com>
2023-07-11 10:47:58 -0500

    Create destructure_from_array.js

---
+++ destructure_from_array.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} location
+ */
+const getLocationString = location => {
+    //destructure into 2 variables: lat & lng
+    const [lat, lng] = location;
+    return `The latitude is ${lat} and the longitude is ${lng}`;
+}
+
+// Sample usage - do not modify
+const location = [24.235235, 2.5734];
+console.log(getLocationString(location)); // "The latitude is 24.235235 and the longitude is 2.5734"
