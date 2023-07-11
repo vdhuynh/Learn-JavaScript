ca1b18cb9b60057a8e2ce432f70b6df4f9cfed7f
Van Huynh <27980695+vdhuynh@users.noreply.github.com>
2023-07-11 17:01:47 -0500

    Create family_group.js

---
+++ family_group.js
@@ -0,0 +1,27 @@
+/**
+ * @param {Object[]} groups
+ * @param {number} groups.id
+ * @param {string} groups.title
+ * @param {object} groups.details
+ * @param {number} groups.details.messageCount
+ * @param {boolean} groups.details.public
+ */
+const getFamilyGroup = groups => groups.find(group => group.title === "Family");
+
+// Sample usage - do not modify
+const sampleGroups = [{
+    id: 1,
+    title: "Football",
+    details: {
+        messageCount: 30,
+        public: true
+    }
+}, {
+    id: 2,
+    title: "Family",
+    details: {
+        messageCount: 1014,
+        public: false
+    }
+}];
+console.log(getFamilyGroup(sampleGroups));
