593d53c581de4726ac95aeb222f57dd2103827ac
Van Huynh <27980695+vdhuynh@users.noreply.github.com>
2023-07-11 13:23:30 -0500

    Create refactoring_property_access.js

---
+++ refactoring_property_access.js
@@ -0,0 +1,15 @@
+/**
+ * @param {object} user
+ * @param {object} [user.payment]
+ * @param {object} [user.payment.details]
+ * @param {number} [user.payment.details.value]
+ */
+const getPaymentValue = user => {{
+        return user?.payment?.details?.value;
+    }
+    return undefined;
+}
+
+// Sample usage - do not modify
+console.log(getPaymentValue({id: 1, name: "Alex"})); // undefined
+console.log(getPaymentValue({id: 2, name: "Sam", payment: {details: {value: 59}}})); // 59
