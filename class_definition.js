75c908128c6e6ec5650d0e7818e79777f49be2e8
Van Huynh <27980695+vdhuynh@users.noreply.github.com>
2023-07-12 11:12:43 -0500

    Create class_definition.js

---
+++ class_definition.js
@@ -0,0 +1,16 @@
+// class definition
+// TODO: define the class here
+class User {
+    constructor(firstName, lastName, age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+
+    }
+}
+
+
+
+// Sample usage - do not modify
+console.log(new User("Sam", "Blue", 18));
+console.log(new User("Alex", "Green", 25));
