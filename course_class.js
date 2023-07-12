6b023045165d646193a6235231f8d4d7861fda86
Van Huynh <27980695+vdhuynh@users.noreply.github.com>
2023-07-12 11:13:53 -0500

    Create course_class.js

---
+++ course_class.js
@@ -0,0 +1,14 @@
+// class definition
+// TODO: define the class here
+
+class Course {
+    constructor(name, isCompleted) {
+        this.name = name;
+        this.isCompleted = isCompleted;
+    }
+}
+
+
+// Sample usage - do not modify
+console.log(new Course("Learn JavaScript", false));
+console.log(new Course("Learn Programming", true));
